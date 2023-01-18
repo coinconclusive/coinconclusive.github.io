efe8659515a32f40c0a358ad2bb804da032926b5
waffle-annie <annie@waffle.land>
2023-01-18 23:43:50 +0300

    maybe finished blog gen

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const fm = require('front-matter');
 const marked = require('marked');
+// const { Console } = require('console');
+// const console = new Console({
+//   stdout: process.stdout,
+//   stderr: process.stderr,
+//   colorMode: "auto",
+//   groupIndentation: 2
+// });
+
+console.group("ahh, you need a blog! will do :)");
 
 const configFile = './ahh.json';
 
@@ -11,13 +20,14 @@ if(!fs.existsSync(configFile) || !fs.statSync(configFile).isFile()) {
   process.exit(1);
 }
 
-console.log(`found config file: ${configFile}`);
+console.log(`Found config file: ${configFile}`);
 
 /**
  * @type {{
  *   public: string,
  *   sources: string,
  *   blog: string,
+ *   template: { blog: string, static: string[] }
  *   index: { source: string, html: string }
  * }} 
 */
@@ -52,26 +62,31 @@ checkObjectKey(configJson, `template.blog`);
 const config = {
   sourcesDir: configJson.sources,
   publicDir: configJson.public,
-  blogDir: path.join(configJson.sources, configJson.blog),
+  blogDir: configJson.blog,
   sourceIndex: configJson.index.source,
   htmlIndex: configJson.index.html,
-  blogTemplate: configJson.template.blog,
+  blogTemplate: path.join(configJson.sources, configJson.template.blog),
+  staticTemplateExts: configJson.template.static,
 };
-
 console.log(`sources: ${config.sourcesDir}`);
 console.log(`public: ${config.publicDir}`);
-console.log(`blog: ${config.blogDir}`);
+console.log(`blog: ${path.join(config.sourcesDir, config.blogDir)}`);
 
-function* walkDir(dir) {
-  for(const f of fs.readdirSync(dir)) {
-    const ff = path.join(dir, f);
-    if(fs.statSync(ff).isDirectory())
-      yield* walkDir(ff);
-    else yield ff;
+const sources = [];
+console.group(`Reading sources from ${config.sourcesDir}`);
+for(const file of fs.readdirSync(config.sourcesDir)) {
+  const fileFull = path.join(config.sourcesDir, file);
+  console.group(`Reading file ${fileFull}`);
+  if(file == path.basename(config.blogDir)) {
+    console.log(`Blog directory (${file}), skipping.`);
+    console.groupEnd();
+    continue;
   }
+  sources.push(file);
+  console.groupEnd();
 }
 
-/**
+  /**
  * @type {{[id: string]: {
  *   author: string,
  *   date: Date,
@@ -84,8 +99,9 @@ function* walkDir(dir) {
  */
 const postIndex = {};
 
-for(const blogDir of fs.readdirSync(config.blogDir)) {
-  const fullBlogDir = path.join(config.blogDir, blogDir);
+console.group(`Reading blog sources from ${path.join(config.sourcesDir, config.blogDir)}`);
+for(const blogDir of fs.readdirSync(path.join(config.sourcesDir, config.blogDir))) {
+  const fullBlogDir = path.join(config.sourcesDir, config.blogDir, blogDir);
   if(fs.statSync(fullBlogDir).isDirectory()) {
     console.log(`- blog post: ${blogDir} (${fullBlogDir})`);
     const sourceIndexFile = path.join(fullBlogDir, config.sourceIndex);
@@ -94,7 +110,7 @@ for(const blogDir of fs.readdirSync(config.blogDir)) {
       console.error("source index file not found, skipping.");
       continue;
     }
-    const source = fs.readFileSync(sourceIndexFile);
+    const source = fs.readFileSync(sourceIndexFile).toString();
     const { attributes, body } = fm(source);
     const html = marked.marked(body);
     
@@ -112,23 +128,159 @@ for(const blogDir of fs.readdirSync(config.blogDir)) {
 
     const files = [];
     for(const f of fs.readdirSync(fullBlogDir)) {
-      files.push(path.join(ff, f));
+      files.push(f);
     }
 
     postIndex[blogDir] = { id: blogDir, html, files, ...metadata };
   }
 }
 
+function remkdirMaybeSync(p) {
+  if(fs.existsSync(p)) {
+    if(!fs.statSync(p).isDirectory()) {
+      console.error(`${p} is not a directory`);
+      process.exit(1);
+    }
+    console.log(`Removing directory ${p}`);
+    fs.rmSync(p, { force: true, recursive: true });
+  }
+  console.log(`Creating directory ${p}`);
+  fs.mkdirSync(p);
+}
+
+function mkdirMaybeSync(p) {
+  if(!fs.existsSync(p)) {
+    fs.mkdirSync(p);
+  } else if(!fs.statSync(p).isDirectory()) {
+    console.error(`${p} is not a directory`);
+    process.exit(1);
+  }
+}
+
+if(!fs.existsSync(config.blogTemplate) || !fs.statSync(config.blogTemplate).isFile()) {
+  console.error(`Blog template file not found (${config.blogTemplate}), aborting`);
+  process.exit(1);
+} else {
+  console.log(`Blog template file found (${config.blogTemplate})`);
+}
+
+console.groupEnd();
+console.group(`Generating static content to ${config.publicDir}`);
+
 /**
  * @param {string} source
  * @return {string}
  */
-function processHtml(source, $) {
-  return new Function('$', 'return `' + source.replace(/`/g, '\\`') + '`')($);
+ function processTemplate(source, context) {
+  const sortedPostIndex = Object.values(postIndex).sort((a, b) => a.date - b.date);
+  const index = sortedPostIndex.findIndex(post => context ? (post.id == context.id) : false);
+  context = {
+    ...context,
+    nav: index < 0 ? undefined : {
+      previous: index > 0 ? sortedPostIndex[index - 1].id : undefined,
+      next: index < sortedPostIndex.length - 1 ? sortedPostIndex[index + 1].id : undefined,
+    },
+    postIndex: sortedPostIndex,
+    config
+  };
+  const entries = Object.entries(context);
+  return new Function(...entries.map(([k, _]) => k), 'return `' + source + '`')
+    (...entries.map(([_, v]) => v));
 }
 
-for(const [id, post] of Object.entries(postIndex)) {
-  const indexPath = path.join(config.blogDir, post.id, config.htmlIndex);
-  console.log(`writing post ${id}: ${indexPath}`);
-  fs.writeFileSync(indexPath, processHtml(post.html));
+const blogHtmlTemplate = fs.readFileSync(config.blogTemplate).toString();
+
+remkdirMaybeSync(config.publicDir);
+mkdirMaybeSync(path.join(config.publicDir, config.blogDir));
+
+console.group(`Generating files to ${path.join(config.publicDir, config.blogDir)}`);
+for(const file of sources) {
+  const destination = path.join(config.publicDir, file);
+  const source = path.join(config.sourcesDir, file);
+  if(config.staticTemplateExts.includes(path.extname(file))) {
+    console.group(`Generating ${source} to ${destination}`);
+    if(!fs.statSync(source).isFile()) {
+      console.error(`${file} is not a file, skipping`);
+      console.groupEnd();
+      continue;
+    }
+    const content = fs.readFileSync(source).toString();
+    fs.writeFileSync(destination, processTemplate(content));
+    console.groupEnd();
+  } else {
+    console.group(`Copying from ${source} to ${destination}`)
+
+    if(fs.existsSync(destination)) {
+      console.log(`Removing old ${destination}`);
+      fs.rmSync(destination);
+    }
+
+    if(file == config.sourceIndex) {
+      console.log(`Source file (${file}), skipping`);
+      console.groupEnd();
+      continue;
+    }
+
+    console.groupEnd();
+
+    fs.cpSync(source, destination, { recursive: true });
+  }
 }
+console.groupEnd();
+
+console.group(`Generating blog to ${path.join(config.publicDir, config.blogDir)}`);
+for(const [id, post] of Object.entries(postIndex)) {
+  mkdirMaybeSync(path.join(config.publicDir, config.blogDir, post.id));
+  const indexPath = path.join(config.publicDir, config.blogDir, post.id, config.htmlIndex);
+  console.group(`Writing post content (${id}) to ${indexPath}`);
+  const postContent = processTemplate(post.html, post);
+  fs.writeFileSync(indexPath, processTemplate(blogHtmlTemplate, { ...post, postContent }));
+  
+  {
+    const source = path.join(config.sourcesDir, config.blogDir, config.htmlIndex)
+    const destination = path.join(config.publicDir, config.blogDir, config.htmlIndex);
+    console.group(`Copying from ${source} to ${destination}`)
+
+    if(!fs.existsSync(source)) {
+      console.log(`Blog index (${source}) does not exist, skipping.`);
+    } else {
+      if(fs.existsSync(destination)) {
+        console.log(`Removing old ${destination}`);
+        fs.rmSync(destination);
+      }
+
+      if(!fs.statSync(source).isFile()) {
+        console.error(`${file} is not a file, skipping`);
+      } else {
+        const content = fs.readFileSync(source).toString();
+        fs.writeFileSync(destination, processTemplate(content));
+      }
+    }
+    console.groupEnd();
+  }
+
+  for(const file of post.files) {
+    const destination = path.join(config.publicDir, config.blogDir, post.id, file);
+    const source = path.join(config.sourcesDir, config.blogDir, post.id, file)
+    console.group(`Copying from ${source} to ${destination}`)
+
+    if(fs.existsSync(destination)) {
+      console.log(`Removing old ${destination}`);
+      fs.rmSync(destination);
+    }
+
+    if(file == config.sourceIndex) {
+      console.log(`Source file (${file}), skipping`);
+      console.groupEnd();
+      continue;
+    }
+
+    console.groupEnd();
+
+    fs.cpSync(source, destination, { recursive: true });
+  }
+  console.groupEnd();
+}
+
+console.groupEnd();
+console.groupEnd();
---
+++ sources/blog/test2/index.md
@@ -0,0 +1,10 @@
+---
+title: "Test post #2"
+description: More testing
+date: 2023-01-18T20:11:42.035Z
+author: Anne Chadeeva
+---
+
+# Checking if the UI works
+
+lorem ipsum dolor sit amet
